c54e9196bb0083e75b12c338df56ab1edb9b83f1
sanaa <Sanaakumar07@gmail.com>
2025-11-19 21:37:55 +0530

    c

--- day 27.1/main.c
+++ day 27.1/main.c
@@ -1,15 +1,19 @@
-//
-//  main.c
-//  day 27.1
-//
-//  Created by Sanaa Kumar on 19/11/25.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void) {
+    int i, j, n = 5;
+
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= 2*i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    for(i = n-1; i >= 1; i--) {
+        for(j = 1; j <= 2*i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
 }
